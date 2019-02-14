57708be5e46181631463e5059c67ee9c703ec13c
Wilson Hung <wihung@microsoft.com>
2019-02-13 22:55:28 -0800

    Adding defaults for expiry time

---
+++ app/Core/Token.py
@@ -0,0 +1,13 @@
+import datetime
+
+class Token:
+    def __init__(self, refresh_method, refresh_token, access_token="", expiry_time=None):
+        self.refresh_method = refresh_method
+        self.refresh_token = refresh_token
+        self.expiry_time = expiry_time
+        self.access_token = access_token
+    
+    def get(self):
+        if self.expiry_time and datetime.utcnow() <= self.expiry_time:
+            self.access_token = self.refresh_method()
+        return self.access_token
