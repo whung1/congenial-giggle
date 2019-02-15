dc8672e53a327a0e0ab5669f37926b2a1572f0bc
Wilson Hung <wihung@microsoft.com>
2019-02-15 00:21:28 -0800

    adding request url method and httpclient

--- app/Mirrors/Imgur/Client.py
+++ app/Mirrors/Imgur/Client.py
@@ -4,14 +4,13 @@ import requests
 
 from app.Core.HttpClient import HttpClient
 
-
 class Client:
     auth_base_url = "https://api.imgur.com/oauth2/token"
 
-    def __init__(self, client_id, client_secret):
+    def __init__(self, client_id, client_secret, http_client):
         self.client_id = client_id
         self.client_secret = client_secret
-        self.http_client = HttpClient()
+        self.http_client = http_client
 
     def refresh_method(self, refresh_token):
         """Use a refresh token to get a new access token from the api and return it"""
@@ -26,8 +25,19 @@ class Client:
             request = self.http_client.post(self.auth_base_url, data=params)
             if request.ok:
                 json_content = request.json()
-                return json_content["access_token"], datetime.utcnow() + timedelta(seconds=json_content["expires_in"])
+                access_token, expiry_time = json_content["access_token"], datetime.utcnow() + timedelta(seconds=json_content["expires_in"]-300)
+                return access_token, expiry_time
             else:
                 return "", datetime.utcnow()
 
         return get_access_token(params)
+
+    def get_refresh_token_request_url(self, response_type="POST", application_state=""):
+        """Generates the url necessary for a user to use to get refresh tokens
+        https://api.imgur.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&response_type=REQUESTED_RESPONSE_TYPE&state=APPLICATION_STATE
+        """
+        return self.auth_base_url + "/authorize?client_id={cid}&response_type={resp}&state={app_state}".format(
+                    cid=self.client_id,
+                    resp=response_type,
+                    app_state=application_state
+                )
